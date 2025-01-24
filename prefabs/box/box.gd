51bbf22f582ea867ac532c4718feb2e845fde6d3
jaakop <jaakko@kronogra.fi>
2025-01-24 18:55:09 +0200

    Added m box to scene

---
+++ prefabs/box/box.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+@onready var animPlayer: AnimationPlayer = $AnimationPlayer;
+@onready var containerArea: Area3D = $BoxContainerArea;
+
+var itemsInTheBox: Array[DragableObject] = [];
+
+func _ready():
+	containerArea.body_entered.connect(onObjectEntered);
+	containerArea.body_exited.connect(onObjectRemoved);
+	open();
+	pass
+
+func _process(delta):
+	pass
+
+func open():
+	animPlayer.play("Open");
+
+func close():
+	animPlayer.play_backwards("Open");
+
+func onObjectEntered(body: Node3D):
+	if(body is not DragableObject):
+		return;
+
+	itemsInTheBox.append(body);
+	pass
+
+func onObjectRemoved(body: Node3D):
+	if(body is not DragableObject):
+		return;
+	var itemIndex = itemsInTheBox.find(body);		
+	if(itemIndex >= 0):
+		itemsInTheBox.remove_at(itemIndex);
+	pass
