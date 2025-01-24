d7e9d9483a3c92e0bbf37822e3cb81cdc5c19b54
jaakop <jaakko@kronogra.fi>
2025-01-24 15:37:55 +0200

    Started moving items on belt

--- prefabs/itemConveyor/item_conveyor.gd
+++ prefabs/itemConveyor/item_conveyor.gd
@@ -5,6 +5,11 @@ class_name ItemConveyor
 @onready var itemHolder = $ItemHolder;
 
 @export var items: Array[PackedScene];
+var itemsOnBelt: Array[Node3D];
+var itemsMoving = {};
+
+@export var itemSpacing: float = 1
+@export var conveyorSpeed: float = 1;
 
 var timer: float;
 var time = 2;
@@ -20,11 +25,22 @@ func _process(delta):
     if(timer < 0):
         timer = time;
         SpawnItem();
+
+    for item : RigidBody3D in itemsMoving:
+        var targetPos = itemsMoving[item];
+        if(item.position.x >= targetPos.x):
+            continue;
+        item.move_and_collide(Vector3(conveyorSpeed * delta,0 ,0));
+
+        pass
     pass
 
 func SpawnItem():
-    var nextItem: Node3D = items.pick_random().instantiate();
+    var nextItem: RigidBody3D = items.pick_random().instantiate() as RigidBody3D;
     nextItem.position = spawnPoint.position;
     itemHolder.add_child(nextItem);
+
+    itemsOnBelt.append(nextItem);
+    itemsMoving[nextItem] = position - Vector3(itemsOnBelt.size() * itemSpacing,0,0);
     
     pass
