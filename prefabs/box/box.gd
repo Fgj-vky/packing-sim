239752ee02489ff793743cad55a3d68bc6d983d7
jaakop <jaakko@kronogra.fi>
2025-01-24 20:03:48 +0200

    Box records its fullness

--- prefabs/box/box.gd
+++ prefabs/box/box.gd
@@ -4,7 +4,10 @@ class_name Box
 @onready var animPlayer: AnimationPlayer = $AnimationPlayer;
 @onready var containerArea: Area3D = $BoxContainerArea;
 
+@export var containerSize: int = 3;
+
 var itemsInTheBox: Array[DragableObject] = [];
+var fillAmount: int = 0;
 
 func _ready():
 	containerArea.body_entered.connect(onObjectEntered);
@@ -23,14 +26,26 @@ func close():
 func onObjectEntered(body: Node3D):
 	if(body is not DragableObject):
 		return;
-
-	itemsInTheBox.append(body);
+	
+	var item = body as DragableObject
+	itemsInTheBox.append(item);
+	fillAmount += item.itemSize;
+	print("box is " + str(fillPrecentage()) + "%% full!");
 	pass
 
 func onObjectRemoved(body: Node3D):
 	if(body is not DragableObject):
 		return;
-	var itemIndex = itemsInTheBox.find(body);		
+		
+	var item = body as DragableObject
+	var itemIndex = itemsInTheBox.find(item);		
 	if(itemIndex >= 0):
 		itemsInTheBox.remove_at(itemIndex);
+		fillAmount -= item.itemSize;
+		print("box is " + str(fillPrecentage() * 100) + "%% full!"); 
 	pass
+
+func isBoxTooFull() -> bool:
+	return fillAmount > containerSize;
+func fillPrecentage() -> float:
+	return fillAmount as float / containerSize;
