0a365902e6cf9fcc6fc280ca8de862a5b99e41f5
jaakop <jaakko@kronogra.fi>
2025-01-24 20:09:21 +0200

    remove debug code

--- prefabs/gameController/game_controller.gd
+++ prefabs/gameController/game_controller.gd
@@ -38,8 +38,6 @@ func sendBoxAway():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(1).timeout;
-	createNewMBox();
 	pass # Replace with function body.
 
 
