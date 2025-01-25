7203cccb0730e6a6c8615480db146a8105dcf171
jaakop <jaakko@kronogra.fi>
2025-01-25 16:12:22 +0200

    Clock

---
+++ prefabs/clock/clock.gd
@@ -0,0 +1,9 @@
+extends DragableObject
+
+@onready var clockHand: MeshInstance3D = $MeshInstance3D/ClockHandMesh
+@export var gameController: GameController
+
+func _process(delta):
+    print(clockHand.rotation.z)
+    clockHand.rotation.z = lerp(PI + PI/2.0, 0.0, 1 - gameController.dayTimer.time_left / gameController.dayTimer.wait_time)
+    pass
